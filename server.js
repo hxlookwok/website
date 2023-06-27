5443616f122b19ca3e3c6fd1638769f420f6047f
hxlookwok <80060842+hxlookwok@users.noreply.github.com>
2023-06-26 18:31:10 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.use(express.static(".public"));
+app.use(express.static("./public"));
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/main.html");
 });
