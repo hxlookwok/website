4b3e10db1c323b883182aaf9ce287874e0df2d76
hxlookwok <80060842+hxlookwok@users.noreply.github.com>
2023-06-26 18:23:18 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+app.use(express.static(".public"));
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/main.html");
+});
+app.post("/", function(req,res) {
+    res.sendFile(__dirname + "/main.css");
+});
+app.listen(process.enc.PORT||3000, function() {
+    console.log(`Server is running on port ${process.env.PORT}.`);
+});
