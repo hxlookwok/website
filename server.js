01dc6ec54680ee7beddd3e1204c8301f51349815
hxlookwok <80060842+hxlookwok@users.noreply.github.com>
2023-06-29 23:31:10 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ app.use(express.static("./public"));
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/main.html");
 });
-app.post("/", function(req,res) {
-    res.sendFile(__dirname + "/main2.html");
-});
 app.listen(process.env.PORT||3000, function() {
     console.log(`Server is running on port ${process.env.PORT}.`);
 });
