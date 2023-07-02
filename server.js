c2f54e6d72d8f34039eeee67b5124ff16c231d24
hxlookwok <80060842+hxlookwok@users.noreply.github.com>
2023-07-02 14:32:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.static("./public"));
-app.get("/", function(req,res){
+app.get("/", function(req, res) {
     res.sendFile(__dirname + "/main.html");
 });
 app.listen(process.env.PORT||3000, function() {
