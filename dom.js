b46314a57a5f3e4bbf436dcc130a9e0205864398
hxlookwok <80060842+hxlookwok@users.noreply.github.com>
2023-07-04 17:43:29 -0700

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
 const submit = document.getElementById("sub")[0];
 submit.addEventListener("click", function() {
-    alert("Your answer has been submitted!);
+    alert("Your answer has been submitted!");
           });
